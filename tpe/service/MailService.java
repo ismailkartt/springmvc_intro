a80a94406263c7b02177557f1d0c174060b82d8d
Ismail Kart <ismailkartt@gmail.com>
2023-01-08 15:33:27 +0300

    08/01/2023

---
+++ tpe/service/MailService.java
@@ -0,0 +1,40 @@
+package com.tpe.service;
+
+import com.tpe.domain.Message;
+import com.tpe.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("mailService")
+@Scope("prototype") // getBean() her çağrıldığında yeni obje getir.
+public class MailService implements MessageService{
+
+    // Field Injection
+//    @Autowired // Dependency Injection
+//    @Qualifier("fileRepository")
+//    private Repository repository;
+
+    // Setter Injection
+//    private Repository repository;
+//
+//    @Autowired
+//    @Qualifier("fileRepository")
+//    public void setRepository(Repository repository) {
+//        this.repository = repository;
+//    }
+
+    //Constructor Injection
+    private Repository repository;
+    @Autowired // Trick : Eğer class içinde 1 tane constructor varsa @Autowired a gerek yok.
+    public MailService(@Qualifier("fileRepository") Repository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void sendMessage(Message message) {
+        System.out.println("Ben Mail servisiyim, size mesaj gönderiyorum : " + message.getMessage());
+        repository.saveMessage(message);
+    }
+}
